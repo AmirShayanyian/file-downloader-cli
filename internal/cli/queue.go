2a4082b776069cb7f2ddffb764dfdbe26bebcd17
AmirShayanyian <wahtamir12@gmail.com>
2025-12-26 00:38:36 +0330

    feat: add create queue method and added to queueCmd

---
+++ internal/cli/queue.go
@@ -0,0 +1,41 @@
+package cli
+
+import (
+	"fmt"
+
+	"github.com/AmirShayanyian/file-downloader-cli/internal/queue"
+	"github.com/AmirShayanyian/file-downloader-cli/internal/storage"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	queueCmd := &cobra.Command{
+		Use:   "queue",
+		Short: "Manage download queues",
+	}
+	queueCmd.AddCommand(createQueueCmd())
+
+	rootCmd.AddCommand(queueCmd)
+}
+
+func createQueueCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "create [name]",
+		Short: "Create a new queue",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			store := storage.NewJSONStore("data/queues.json")
+
+			q := queue.Queue{
+				Name: args[0],
+			}
+
+			if err := store.Save(q); err != nil {
+				return err
+			}
+
+			fmt.Println("Queue created:", q.Name)
+			return nil
+		},
+	}
+}
