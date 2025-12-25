9aecc3e7367739267043600b462f44b6d04a74fa
AmirShayanyian <wahtamir12@gmail.com>
2025-12-26 00:10:43 +0330

    feat: implement list method from repository

--- internal/storage/json_storage.go
+++ internal/storage/json_storage.go
@@ -1,9 +1,34 @@
 package storage
 
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/AmirShayanyian/file-downloader-cli/internal/queue"
+)
+
 type JSONStore struct {
 	path string
 }
 
 func NewJSONStore(path string) *JSONStore {
 	return &JSONStore{path: path}
+}
+
+func (s *JSONStore) List() ([]queue.Queue, error) {
+	if _, err := os.Stat(s.path); os.IsNotExist(err) {
+		return []queue.Queue{}, nil
+	}
+
+	data, err := os.ReadFile(s.path)
+	if err != nil {
+		return nil, err
+	}
+
+	var queues []queue.Queue
+	if err := json.Unmarshal(data, &queues); err != nil {
+		return nil, err
+	}
+
+	return queues, nil
 }
